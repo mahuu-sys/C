d379a84911bade2ee76c385b9c70c20a8e2b1e8a
mahuu-sys <mahinshah717@gmail.com>
2025-06-25 16:12:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    char ch;
+    char name[100];
+    printf("\enter name=> ");
+    gets(name);
+    int length = strlen(name);
+    int i;
+
+    for (int i = length - 1; i >= 0; i--) {
+        ch=name[i];
+        printf("%c",ch);
+    }
     return 0;
 }
+
