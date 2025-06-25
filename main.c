505f87b5b3e3d4b8fc3578e1f9f4248f0f866696
mahuu-sys <mahinshah717@gmail.com>
2025-06-25 16:32:20 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main(){
+    int square,no1;
+    printf("enter no1\n");
+    scanf("%d",&no1);
+    square=no1*no1;
+    printf("square=%d",square);
     return 0;
 }
