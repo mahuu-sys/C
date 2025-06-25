b144884c2260fff7afdb17da819dbb69a029ec2e
mahuu-sys <mahinshah717@gmail.com>
2025-06-25 16:26:33 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
-int main() {
-    char ch;
-    char name[100] = {"Mahin"};
-    int length = strlen(name);
-    int i, uppercase = 0, lowercase = 0;
-
-    for (i = 0; i < length; i++) {
-        ch = name[i];
-        if (isupper(ch)) {
-            uppercase++;
-        } else if (islower(ch)) {
-            lowercase++;
-            printf("%c",ch);
-        }
-    }
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
     return 0;
 }
-
