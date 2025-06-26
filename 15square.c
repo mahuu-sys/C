687760278ada40f13f9069fe1829123ae9ac64af
mahuu-sys <mahinshah717@gmail.com>
2025-06-26 20:54:40 +0530

    Add files via upload

---
+++ 15square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int number,square,cube;
+	printf("\ngive the number");
+	scanf("%d",&number);
+	square=number*number;
+	cube=number*number*number;
+	printf("\nsquare=%d",square);
+	printf("\ncube=%d",cube);
+	
+}
---
+++ 16cm,km.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int m,cm;
+	printf("\ngive the meter ");
+	scanf("%d",&m);
+	cm=m*100;
+	printf("\ncm=%d",cm);
+}
