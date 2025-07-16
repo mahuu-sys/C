ab578fac844943755f1b581879b737d723cc645c
mahuu-sys <mahinshah717@gmail.com>
2025-07-16 23:22:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name1[20]={"manav"};
+    char name2[20]={"mansi"};
+
+    printf("\n%s\n", name1);
+    printf("\n%s\n", name2);
+
+    strcat(name1,name2);
+    
+    printf("\n%s\n", name1);
+    printf("\n%s\n", name2);
+
+    return 0;
+}
+
