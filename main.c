3f817044013ecbc2213160913755bf184cd45bc8
mahuu-sys <mahinshah717@gmail.com>
2025-07-16 23:15:33 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char name[100] = "Mahin";
+
+    for (int i = 0; name[i]; i++) {
+        name[i] = tolower(name[i]);
+    }
+
+    printf("\n%s\n", name);
+    return 0;
+}
+
